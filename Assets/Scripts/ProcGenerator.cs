8f86b9386adac3c64c73374e2b564a428f26614e
Noelle Yeomans <90638405+NoelleFYeomans@users.noreply.github.com>
2023-10-31 09:54:39 -0300

    Created premade rooms

--- Assets/Scripts/ProcGenerator.cs
+++ Assets/Scripts/ProcGenerator.cs
@@ -2,8 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProcGenerator : MonoBehaviour
+public class ProcGenerator : MonoBehaviour //TODO List: make Algorithm lmao
 {
+    //Below is the room prefabs
+    [Header("Types of Rooms")]
+    public GameObject fourWay;
+    [Header("Three Way Rooms")]
+    public GameObject threeWayNES;
+    public GameObject threeWayESW;
+    public GameObject threeWaySWN;
+    public GameObject threeWayWNE;
+    [Header("Two Way Rooms")]
+    public GameObject twoWayNS;
+    public GameObject twoWayEW;
+    [Header("Dead Ends")]
+    public GameObject oneWayN;
+    public GameObject oneWayE;
+    public GameObject oneWayS;
+    public GameObject oneWayW;
+    [Header("Corners")]
+    public GameObject cornerNE;
+    public GameObject cornerSE;
+    public GameObject cornerSW;
+    public GameObject cornerNW;
+
     // Start is called before the first frame update
     void Start()
     {
